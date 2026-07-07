ccffab2e818a2b1447d110b136cd364ac50bc040
Artur Zelik <zel1916@o2.pl>
2026-07-07 14:01:12 +0200

    add a condition to check if the gripper is turned on

--- scripts/run_teleop.sh
+++ scripts/run_teleop.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+spawn_franka_left=true
+spawn_franka_right=true
+
 pixi run -e humble \
-ros2 launch franka_meta_quest start.launch.py spawn_franka_left:=false  spawn_franka_right:=true
+ros2 launch franka_meta_quest start.launch.py spawn_franka_left:=$spawn_franka_left  spawn_franka_right:=$spawn_franka_right
